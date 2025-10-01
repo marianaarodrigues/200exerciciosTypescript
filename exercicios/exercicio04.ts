5b568ddca034f21e3d8c4b4cd667f18edd718346
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 11:03:41 -0300

    Declara duas variáveis, 'nome' e 'sobrenome', atribui valores, concatena em uma terceira variável, 'nomeCompleto', e imprime

---
+++ exercicios/exercicio04.ts
@@ -0,0 +1,11 @@
+/*
+
+Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. 
+Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+*/
+
+const nome: string = "Mariana";
+const sobrenome: string = "Rodrigues";
+const nomeCompleto: string = nome + " " + sobrenome;
+console.log(`Nome: ${nomeCompleto}.`);
