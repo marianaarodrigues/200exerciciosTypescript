84ec83308d17839ac3952dc4ab18be52990eebe3
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 10:46:42 -0300

    Declara duas variáveis, 'idade' e 'peso', atribui valores, e imprime na console

--- exercicios/exercicio01.ts
+++ exercicios/exercicio01.ts
@@ -1,6 +1,6 @@
 /*
 
-Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima na console.
 
 */
 
