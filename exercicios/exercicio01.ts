1203090572dd09fcb2119ac6b7bba3848c09f5c2
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 10:43:53 -0300

    Adicionando a pasta exercicios

---
+++ exercicios/exercicio01.ts
@@ -0,0 +1,10 @@
+/*
+
+Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+
+*/
+
+const idade: number = 33;
+const peso: number = 57;
+
+console.log(`Idade: ${idade} anos, e peso: ${peso} kg.`);
