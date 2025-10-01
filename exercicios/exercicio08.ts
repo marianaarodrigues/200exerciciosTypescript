464eb435caf54c005dff901b46b5b94bbe1ea1d7
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 11:53:41 -0300

    Declara uma variável e atribua um valor numérico, usa o operador de incremento, e imprime o resultado

---
+++ exercicios/exercicio08.ts
@@ -0,0 +1,9 @@
+/*
+
+Declare uma variável e atribua um valor numérico a ela. 
+Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.
+
+*/
+
+let num: number = 10;
+console.log(`++${num} = ${++num}`);
