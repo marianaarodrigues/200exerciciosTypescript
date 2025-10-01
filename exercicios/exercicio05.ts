15df33abdac3b1a3cffe8b8e171b1d260f28d2f3
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 11:07:37 -0300

    Declara duas variáveis e atribui valores numéricos, usa o operador de adição, e imprime o resultado

---
+++ exercicios/exercicio05.ts
@@ -0,0 +1,10 @@
+/*
+
+Declare duas variáveis e atribua valores numéricos a elas. 
+Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+*/
+
+const num1: number = 5;
+const num2: number = 10;
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
