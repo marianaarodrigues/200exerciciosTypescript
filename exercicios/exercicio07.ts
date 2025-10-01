4c700f21a0f1a085b037f2bf9cc53fb211faf6a5
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 11:46:28 -0300

    Declara três variáveis e atribui valores numéricos, usa operadores de comparação, e imprime

---
+++ exercicios/exercicio07.ts
@@ -0,0 +1,11 @@
+/*
+
+Declare três variáveis e atribua valores numéricos a elas. 
+Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+*/
+
+const numero1: number = 0;
+const numero2: number = -5;
+const numero3: number = 20;
+console.log(`${numero2} < ${numero1} < ${numero3}`);
