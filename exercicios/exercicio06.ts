fb4d25bcdd682f8dae7d3b39b05a7456d23fbd4e
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 11:41:00 -0300

    Declara duas variáveis e atribui valores booleanos, usa os operadores lógicos AND, OR e NOT, e imprime os resultados

---
+++ exercicios/exercicio06.ts
@@ -0,0 +1,13 @@
+/*
+
+Declare duas variáveis e atribua valores booleanos a elas. 
+Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.
+
+*/
+
+const var1: boolean = true;
+const var2: boolean = false;
+console.log(`${var1} AND ${var2} = ${var1 && var2}`);
+console.log(`${var1} OR ${var2} = ${var1 || var2}`);
+console.log(`NOT ${var1} = ${!var1}`);
+console.log(`NOT ${var2} = ${!var2}`);
