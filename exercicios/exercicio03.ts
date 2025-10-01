c75507abeceb7f978f99920ff892535db4aa44a9
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-10-01 10:56:32 -0300

    Declara uma variável chamada 'numeroMaximo', atribui o valor máximo que um número pode ter em TypeScript, e imprime na console

---
+++ exercicios/exercicio03.ts
@@ -0,0 +1,9 @@
+/*
+
+Declare uma variável chamada "numeroMaximo", atribua a ela o valor máximo que um número pode ter em TypeScript 
+e imprima o valor na console.
+
+*/
+
+const numeroMaximo: number = Number.MAX_VALUE;
+console.log(`O valor máximo que um número pode ter em TypeScript é ${numeroMaximo}.`);
