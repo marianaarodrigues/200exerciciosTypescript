e0cf80bac5dca7139cd25e5d773f613edd34e04f
Mariana Rodrigues <rodriguesma627@gmail.com>
2025-11-02 18:38:12 -0300

    Usando um laço do-while, imprime todos os números pares de 1 a 100

---
+++ src/exercicio29.ts
@@ -0,0 +1,13 @@
+/*
+
+Usando um laço do-while, imprima todos os números pares de 1 a 100.
+
+*/
+
+let i: number = 2;
+do {
+    if(i % 2 ==0) {
+        console.log(i);
+    }
+    i++;
+} while(i <= 100)
